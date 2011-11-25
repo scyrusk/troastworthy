017e583757879a852f9da60a237ee937c3c47011
scyrusk <sauvik@cmu.edu>
2011-11-25 13:22:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Troastworthy::Application.config.secret_token = 'a782c07c9dbee6717553439c4231a2a1119a7d65480703860f6744ff78e60387e2ac425bce8b31d295777676b798034ffdac1c6b7fd247f703e446bbfe1885d5'
