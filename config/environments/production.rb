d16648e2a99ba235a878a9d2d966a53c969d7760
scyrusk <sauvik@cmu.edu>
2011-11-25 15:39:23 -0500

    got rid of Sass line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,4 @@ Troastworthy::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.middleware.use Hassle
-  Sass::Plugin.options[:never_update] = true
 end
